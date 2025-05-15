fd5083c8978af88b18cbf31e40becf0d0548f8a8
Lukas chaves <lucaschavesjr@gmail.com>
2025-05-15 20:23:23 +0100

    Crinado metodo ObterPorId no Controller

--- src/main/java/br/com/lukasprojetos/codechella/controllers/EventoController.java
+++ src/main/java/br/com/lukasprojetos/codechella/controllers/EventoController.java
@@ -5,9 +5,11 @@ import br.com.lukasprojetos.codechella.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/eventos")
@@ -15,9 +17,19 @@ public class EventoController {
     @Autowired
     private EventoService service;
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<EventoDto> obterTodos(){
+    @GetMapping//(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<EventoDto> obterTodos() {
         return service.obterTodos();
-    }
 
+
+    }
+    @GetMapping("/{id}")
+    public Mono<EventoDto> obterPorId(@PathVariable Long id){
+        return service.obterPorId(id);
+
+
+    }
 }
+
+
+
--- src/main/java/br/com/lukasprojetos/codechella/service/EventoService.java
+++ src/main/java/br/com/lukasprojetos/codechella/service/EventoService.java
@@ -3,8 +3,11 @@ package br.com.lukasprojetos.codechella.service;
 import br.com.lukasprojetos.codechella.repositories.EventoRepository;
 import br.com.lukasprojetos.codechella.dto.EventoDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class EventoService {
@@ -14,5 +17,14 @@ public class EventoService {
     public Flux<EventoDto> obterTodos(){
         return repositorio.findAll()
                 .map(EventoDto::toDto);
+
+
+    }
+
+    public Mono<EventoDto> obterPorId(Long id) {
+        return repositorio.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .map(EventoDto::toDto);
     }
 }
+
